23892c7a77d50f0a006170291e033fe6c7339c1e
Tudor Caseru <theoriginaltudor@gmail.com>
2021-05-22 12:19:02 +0200

    add component to app.js

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Grid from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
-import { Product } from "./Product/Product";
+import Product from "./Product/Product";
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$62.99' },
